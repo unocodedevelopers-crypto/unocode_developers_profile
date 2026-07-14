c2f9a1aff2a98c3cec1f7ddecb0b59418ad25f1f
Bavankumar2002 <bavankumar2002@gmail.com>
2026-07-14 17:44:57 +0530

    feat: implement contact page with integrated submission form and site header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -125,61 +125,7 @@ export default function Header() {
                                         <path d="M13.73 4a2 2 0 0 0-3.46 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z" />
 
 
-                                    </svg></span></a></div> <span
-                                        className="h-7 w-0.5 rounded-full bg-white/40"></span>
-                        <div className="flex items-center xl:gap-2"><a className="group relative rounded-sm px-4 py-2"
-                            href="/cdn-cgi/l/email-protection#87ebe2e8e9c7e8e5e2f5eae6e9e9aaf0e2e5e3e2f4eee0e9a9e3e2"
-                            aria-label="Email"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5"
-                                strokeLinecap="round" strokeLinejoin="round"
-                                className="lucide-icon lucide lucide-mail">
-                                <rect width="20" height="16" x="2" y="4" rx="2" />
-                                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
-
-                            </svg> <span
-                                className="pointer-events-none absolute top-1/2 -translate-y-1/2 rotate-90 opacity-0 transition-all duration-200 group-hover:opacity-100 motion-reduce:transition-opacity left-2.5 group-hover:left-0.5 motion-reduce:left-0.5"><svg
-                                    xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 24 24"
-                                    fill="#fff" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
-                                    strokeLinejoin="round" className="lucide-icon lucide lucide-triangle">
-                                    <path d="M13.73 4a2 2 0 0 0-3.46 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z" />
-
-
-                                </svg></span> <span
-                                    className="pointer-events-none absolute top-1/2 -translate-y-1/2 -rotate-90 opacity-0 transition-all duration-200 group-hover:opacity-100 motion-reduce:transition-opacity right-2.5 group-hover:right-0.5 motion-reduce:right-0.5"><svg
-                                        xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 24 24"
-                                        fill="#fff" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
-                                        strokeLinejoin="round" className="lucide-icon lucide lucide-triangle">
-                                    <path d="M13.73 4a2 2 0 0 0-3.46 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z" />
-
-
-                                </svg></span></a><a className="group relative rounded-sm px-4 py-2"
-                                    href="tel:49-156-7954-3182" aria-label="Phone"><svg
-                                        xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 24 24"
-                                        fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"
-                                        strokeLinejoin="round" className="lucide-icon lucide lucide-phone">
-                                    <path
-                                        d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
-
-
-                                </svg> <span
-                                    className="pointer-events-none absolute top-1/2 -translate-y-1/2 rotate-90 opacity-0 transition-all duration-200 group-hover:opacity-100 motion-reduce:transition-opacity left-2.5 group-hover:left-0.5 motion-reduce:left-0.5"><svg
-                                        xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 24 24"
-                                        fill="#fff" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
-                                        strokeLinejoin="round" className="lucide-icon lucide lucide-triangle">
-                                        <path d="M13.73 4a2 2 0 0 0-3.46 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z" />
-
-
-                                    </svg></span> <span
-                                        className="pointer-events-none absolute top-1/2 -translate-y-1/2 -rotate-90 opacity-0 transition-all duration-200 group-hover:opacity-100 motion-reduce:transition-opacity right-2.5 group-hover:right-0.5 motion-reduce:right-0.5"><svg
-                                            xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 24 24"
-                                            fill="#fff" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
-                                            strokeLinejoin="round" className="lucide-icon lucide lucide-triangle">
-                                        <path d="M13.73 4a2 2 0 0 0-3.46 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z" />
-
-
-                                    </svg></span></a>
-
-                        </div>
+                                    </svg></span></a></div>
                     </div>
                 </div>
                     </>
